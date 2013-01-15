9bf29de52471450df158ff53380cd9ed1e316da1
Philippe Traverse <philippe.traverse@gmail.com>
2013-01-15 02:22:11 -0800

    added some useless crap
    work more later

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            f1 = new Form1();
+        }
+
+    }
+}
